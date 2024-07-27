55e456b11f4f5dd84b61e3e2a0c9729933b2665a
Suhani Jain <jainsuhani132@gmail.com>
2024-07-28 02:08:24 +0530

    implemented props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,16 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Layout></Layout> } />
+        <Route path="/" element={ 
+          <Layout>
+          <p>Home Page</p>
+        </Layout>
+       } />
+       <Route path="/search" element={ 
+          <Layout>
+          <p>Search Page</p>
+        </Layout>
+       } />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="bg-purple-900  py-10">
+      <div className="container mx-auto flex flex-col  items-center text-white">
+        <span className=" text-md items-center">
+          MernBooking.com is part of Booking Holdings Inc., the world leader in
+          online travel and related services.
+        </span>
+        <span className="text-md items-center">Copyright © 1996–2024 Booking.com™. All rights reserved.</span>
+
+        <span className=" text-lg flex flex-row gap-5  ">
+          <p className="cursor-pointer hover:text-gray-300">Privacy Policy</p>
+          <p className="cursor-pointer hover:text-gray-300">Terms of Services</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Footer;
