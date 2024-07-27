05640033953205538e3474315069c8af36a4040d
Suhani Jain <jainsuhani132@gmail.com>
2024-07-28 00:31:29 +0530

    setting up layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+ import Layout from "./layouts/Layout";
+// import Register from "./pages/Register";
+// import SignIn from "./pages/SignIn";
+// import AddHotel from "./pages/AddHotel";
+// import { useAppContext } from "./contexts/AppContext";
+// import MyHotels from "./pages/MyHotels";
+// import EditHotel from "./pages/EditHotel";
+// import Search from "./pages/Search";
+// import Detail from "./pages/Detail";
+// import Booking from "./pages/Booking";
+// import MyBookings from "./pages/MyBookings";
+// import Home from "./pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Layout></Layout> } />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
