b7743c6450607b08d3d0cdd39ff01a3f25d69968
Suhani Jain <jainsuhani132@gmail.com>
2024-07-29 21:41:12 +0530

    user registration api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.get("/api/test",async (req: Request, res: Response)=>{
     res.json({message: "hello from express endpoint!"});
 });
+
+app.use("/api/users",userRoutes)
 
 app.listen(7000, ()=>{
     console.log("server is running on localhost: 7000");
