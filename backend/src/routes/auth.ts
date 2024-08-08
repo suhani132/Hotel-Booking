7e0d00224f4f58e92ed269d1bf44acea3694eafd
Suhani Jain <jainsuhani132@gmail.com>
2024-08-09 02:43:49 +0530

    user Login state

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import User from "../models/User";
 import bcrypt from "bcryptjs";
 import jwt from "jsonWebToken";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -56,5 +57,9 @@ router.post(
     }
   }
 );
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response)=>{
+  res.status(200).send({userId: req.userId});
+});
 
 export default router;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = ()=>{
+    const { isLoggedIn} = useAppContext();
+
     return (
         <div className="bg-purple-900 py-6">
             <div className="container mx-auto flex justify-between">
@@ -8,9 +11,17 @@ const Header = ()=>{
                     <Link to="/">MernHolidays.com</Link>
                 </span>
                 <span className="flex space-x-2">
-                    <Link to="/Sign-in" className="flex items-center bg-white text-purple-900 px-3 font-bold hover:bg-purple-200">
+                    {isLoggedIn ? (
+                    <>
+                    <Link to="/my-bookings"> My Bookings</Link>
+                    <Link to="/my-hotels"> My Hotels</Link>
+                    <button>Sign Out</button>
+                    </>
+                    ):( <Link to="/Sign-in" className="flex items-center bg-white text-purple-900 px-3 font-bold hover:bg-purple-200">
                     Sign In
                     </Link>
+                    )}
+                   
                 </span>
             </div>
         </div>
