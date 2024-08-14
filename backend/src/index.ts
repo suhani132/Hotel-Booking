0202a67e0123651e6f479749c2c4622831b1661a
Suhani Jain <jainsuhani132@gmail.com>
2024-08-14 14:33:48 +0530

    end to end test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,35 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
-import userRoutes from './routes/users';
-import authRoutes from './routes/auth';
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
- app.use(cors({  origin: process.env.FRONTEND_URL,
-  credentials: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/test",async (req: Request, res: Response)=>{
-    res.json({message: "hello from express endpoint!"});
+//app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend's origin
+  methods: 'GET, POST, PUT, DELETE', // Allowed methods
+  credentials: true // If you need to send cookies or authentication headers
+}));
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "hello from express endpoint!" });
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(7000, ()=>{
-    console.log("server is running on localhost: 7000");
+
+app.listen(7000, () => {
+  console.log("server is running on localhost: 7000");
 });
